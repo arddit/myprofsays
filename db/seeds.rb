51ace4142a74ab60c676031b066148574aa52a7f
Ardi <ardihamza@gmail.com>
2016-02-17 01:29:58 +0100

    removed seeds

--- db/seeds.rb
+++
@@ -1,5 +0,0 @@
-p1 = Professor.create(name: "Betim Cico")
-p2 = Professor.create(name: "Flamur Thaqi")
-
-Post.create(text: "Mbylle gojen", professor_id: p1.id, user_id: 1)
-Post.create(text: "Cuno, dil jasht", professor_id: p2.id, user_id: 3)
