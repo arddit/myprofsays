3b7c1505e5b9b5c9a6e8fbc12dd262ffbfc791e1
Ardi <ardihamza@gmail.com>
2016-02-19 14:01:19 +0100

    added footer and destroy method for comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -11,6 +11,13 @@ class CommentsController < ApplicationController
 	  end
 	end
 
+	def destroy
+    @post = Post.friendly.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
+    @comment.destroy
+    redirect_to @post
+  end
+
 	private
 
     def comment_params
