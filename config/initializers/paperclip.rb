8951a149371fedcccd45e6b346b394dd0828134d
Ardi <ardihamza@gmail.com>
2016-02-22 21:42:34 +0100

    changed paperclip config for EU AWS Server

---
+++ config/initializers/paperclip.rb
@@ -0,0 +1,2 @@
+Paperclip::Attachment.default_options[:url] = ':myprofsays.s3-website.eu-central-1.amazonaws.com/'
+Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
