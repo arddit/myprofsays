d0d4d18e19ee28a0b0d84588eb97dacb6287802b
Ardi <ardihamza@gmail.com>
2016-02-23 18:00:08 +0100

    testing default image for porfessors

--- app/models/professor.rb
+++ app/models/professor.rb
@@ -1,6 +1,6 @@
 class Professor < ActiveRecord::Base
 	has_many :posts
-  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/assets/:style/professor.png"
+  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/professor.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
 
 	extend FriendlyId
